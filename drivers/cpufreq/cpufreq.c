e2bfe955b124cbdc227308e87bafa6379be0afe0
kevintm78 <kevintm78@gmail.com>
2017-09-19 21:41:14 -0400

    Add Alucard Gov

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -336,14 +336,14 @@ EXPORT_SYMBOL_GPL(cpufreq_notify_transition);
  *
  * This function is called everytime the CPU load is evaluated by the
  * ondemand governor. It notifies userspace of cpu load changes via sysfs.
- */
+ 
 void cpufreq_notify_utilization(struct cpufreq_policy *policy,
 		unsigned int util)
 {
 	if (policy)
 		policy->util = util;
 }
-
+*/
 
 /*********************************************************************
  *                          SYSFS INTERFACE                          *
